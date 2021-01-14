cd163557c217a30d072c9ecbe54b8d795afab1a9
EmirlanDogdurbaev <jigit38@gmail.com>
2021-01-14 20:07:09 +0300

    I start to fix minor mistakes

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+let firstName = "Emirlan";
+let lastName = "Dogdurbaev";
+let placeOfBirth = "Karakol";
+
+alert( "My name is " + firstName + " " + lastName + "," + " I'm from " + placeOfBirth );
+
+let fullName = firstName + " " + lastName;
+
+let message = "My name is" + " " + firstName;
+
+console.log(message);
+
+let message2 = "I'm " + firstName + " from " + placeOfBirth;
+
+console.log(message2);
+
+// let age = 30
+
+let message3 = "My full name is " + fullName + ", " + "I'm from " + placeOfBirth + "." + " I'm " + age + " years old"
+console.log(message3)
